c045a96ca589fa9ff9d7dbf8e97544c08a156340
Moch Riyan Putra Suryadi <88395216+riyanslr10@users.noreply.github.com>
2023-10-30 23:52:56 +0700

    TUGAS LARAVEL

---
+++ storage/framework/views/1f0b4ddfdd224b58e793e6e2319e3b70b30e115e.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>WPU Blog | Home</title>
+</head>
+<body>
+    <h1>Halaman Home</h1>
+
+  <script src="js/script.js"></script>  
+</body>
+</html><?php /**PATH C:\laragon\www\prakweb2023_a_213040017_laravel\resources\views/Home.blade.php ENDPATH**/ ?>
